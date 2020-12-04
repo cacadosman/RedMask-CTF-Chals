eb526fd3753c9f2c20fc122896abd8e8a098e905
Your Name <you@example.com>
2020-12-04 15:02:04 +0700

    edit deploy.sh

--- Penyisihan/Web/WTube/server/deploy.sh
+++ Penyisihan/Web/WTube/server/deploy.sh
@@ -1,3 +1,3 @@
-sudo docker build -t wtube_backend:latest -f Dockerfile.app
-sudo docker build -t wtube_server:latest -f Dockerfile.nginx
+sudo docker build -t wtube_backend:latest -f Dockerfile.app .
+sudo docker build -t wtube_server:latest -f Dockerfile.nginx .
 sudo docker stack deploy --compose-file=docker-compose.yml wtube
